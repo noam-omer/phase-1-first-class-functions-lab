ffb80d20e8a66a95e1dbbabb538372b2e46c3a76
Noam Fischhoff <nfischhoff@gmail.com>
2022-08-13 12:53:34 -0400

    commit tree 2cdc28f5a4690d03d982efdd1e19083a0554c297

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (function(array){
+    return array.slice(0,2);
+})
+
+const returnLastTwoDrivers = (function(array){
+    return array.slice(-2);
+})
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer){
+    return (fare) => fare*integer;
+} 
+
+const fareDoubler =  function(fare){
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare){
+    return createFareMultiplier(3)(fare)
+}    
+
+const selectDifferentDrivers = function(array, inputFunction){
+    if (inputFunction === returnFirstTwoDrivers) return returnFirstTwoDrivers(array);
+    else return returnLastTwoDrivers(array);
+}
+
+const testArray = (['a', 'b', 'c', 'd']);
+console.log(testArray);
+console.log(returnFirstTwoDrivers(testArray));
+console.log(returnLastTwoDrivers(testArray));
